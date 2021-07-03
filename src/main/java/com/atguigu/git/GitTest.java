b189e5ace32838c17d086462b8d558c74b7bb744
xiaolongdong <xiaolongdong75@163.com>
2021-07-03 12:30:59 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,6 +13,8 @@ public class GitTest {
         System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("master test");
+
 
     }
 }
