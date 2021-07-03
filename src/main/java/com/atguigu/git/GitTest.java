1e50d82ab672dc1519b0fa81f7870713c6b47a13
xiaolongdong <xiaolongdong75@163.com>
2021-07-03 11:52:22 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Author：xiaolongdong
+ * @Version：1.0
+ * @Date：2021/7/3-11:49
+ * @Since:jdk1.8
+ * @Description:TODO
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
