2f6bc6df54e0adcf9b842339e1411f61104fdf3d
xiaolongdong <xiaolongdong75@163.com>
2021-07-03 12:27:05 +0800

    hot-fix 1 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,7 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git1");
         System.out.println("hello git2");
+        System.out.println("hello git3");
+
     }
 }
